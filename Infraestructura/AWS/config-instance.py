133c56c582e6f1350c4bd5eddf21e44280c6b1c0
Linkcar13 <carlos.estrada01@epn.edu.ec>
2024-06-26 17:53:58 -0500

    fix(terraform instace config): fix errors on IAC

--- Infraestructura/AWS/config-instance.py
+++ Infraestructura/AWS/config-instance.py
@@ -1,18 +1,32 @@
 import subprocess
 import json
+import os
 import boto3
 
-def run_terraform():
+def run_terraform(terraform_dir):
+    # Cambia el directorio de trabajo a la carpeta de Terraform
+    os.chdir(terraform_dir)
+
     # Inicializa Terraform
     subprocess.run(["terraform", "init"], check=True)
 
     # Aplica la configuración de Terraform y captura la salida
     result = subprocess.run(["terraform", "apply", "-auto-approve", "-json"], capture_output=True, text=True, check=True)
+    
+    # Vuelve al directorio original
+    os.chdir("..")
 
-    # Analiza la salida JSON para obtener el instance_id
-    apply_output = json.loads(result.stdout)
+    # Guardar la salida en una variable para su análisis
+    output = result.stdout
+
+    # Extraer solo la parte JSON de la salida
+    json_output = extract_json_from_output(output)
+
+    # Cargar el JSON extraído
+    apply_output = json.loads(json_output)
+
+    # Buscar el instance_id en el JSON
     instance_id = None
-
     for resource in apply_output['resource_changes']:
         if resource['type'] == 'aws_instance':
             instance_id = resource['change']['after']['id']
@@ -23,30 +37,68 @@ def run_terraform():
 
     return instance_id
 
+def extract_json_from_output(output):
+    """
+    Extraer solo la parte JSON de la salida de Terraform
+    """
+    json_start = output.find('{')
+    json_end = output.rfind('}') + 1
+    json_output = output[json_start:json_end]
+    return json_output
+
 def execute_bash_script(instance_id):
     ssm_client = boto3.client('ssm')
 
-    bash_script = """#!/bin/bash
+    bash_script = """##!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
-apt-get install -y docker.io
+#uninstall versions of docker
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do apt-get remove -y $pkg; done
+#Install docker and dependencies
+apt-get install -y  ca-certificates curl
+sudo install -y  -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update -y
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo groupadd docker
+sudo usermod -aG docker ubuntu
+newgrp docker
 apt-get install -y docker-compose
+systemctl restart docker.socket
 apt-get install -y openssl
 apt-get install -y git
-mkdir -p /hyperledger && cd /hyperledger
+mkdir /home/ubuntu/hyperledger && cd /home/ubuntu/hyperledger
 wget https://github.com/hyperledger/firefly-cli/releases/download/v1.3.0/firefly-cli_1.3.0_Linux_x86_64.tar.gz
-tar -zxf firefly-cli_1.3.0_Linux_x86_64.tar.gz -C /usr/local/bin ff && rm firefly-cli_1.3.0_Linux_x86_64.tar.gz
+sudo tar -zxf /home/ubuntu/hyperledger/firefly-cli_*.tar.gz -C /usr/local/bin ff && rm /home/ubuntu/hyperledger/firefly-cli_*.tar.gz
 curl -sSLO https://raw.githubusercontent.com/hyperledger/fabric/main/scripts/install-fabric.sh && chmod +x install-fabric.sh
 ./install-fabric.sh
 wget https://github.com/hyperledger/fabric/releases/download/v2.5.8/hyperledger-fabric-linux-amd64-2.5.8.tar.gz
-tar -zxf hyperledger-fabric-linux-amd64-2.5.8.tar.gz && cd hyperledger-fabric-linux-amd64-2.5.8
-cd bin && cp * /usr/bin && cd ..
+mkdir /home/ubuntu/hyperledger/hyperledger-binaries
+tar -zxf hyperledger-fabric-linux*.tar.gz -C /home/ubuntu/hyperledger/hyperledger-binaries  && cd /home/ubuntu/hyperledger/hyperledger-binaries
+cd bin && sudo cp * /usr/bin && cd ..
 cd ..
-git clone https://github.com/hyperledger/fabric-samples.git
-mkdir smart-contracts
-cd
-ff init test-prod1 2 -b "fabric" -p 8000
-ff start test-prod1 -v
+#git clone https://github.com/hyperledger/fabric-samples.git
+mkdir /home/ubuntu/hyperledger/smart-contracts
+cd /home/ubuntu/hyperledger/smart-contracts
+#clonar repo de smartcontracts
+cd /home/ubuntu/
+sudo systemctl enable docker.socket
+sudo systemctl restart docker.socket
+sudo systemctl daemon-reload
+sleep 180
+cd /home/ubuntu/
+source .bashrc
+source .profile
+sleep 180
+ff --help
+ff init test 2 -b "fabric" -p 8000
+ff start test -v -b
 """
 
     response = ssm_client.send_command(
@@ -75,6 +127,7 @@ ff start test-prod1 -v
     print(output['StandardErrorContent'])
 
 if __name__ == "__main__":
-    instance_id = run_terraform()
+    terraform_directory = "../Terraform/"  # Directorio que contiene el script de Terraform
+    instance_id = run_terraform(terraform_directory)
     print(f"Created instance with ID: {instance_id}")
     execute_bash_script(instance_id)
